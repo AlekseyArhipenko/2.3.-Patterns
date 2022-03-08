6376a9f71b6e183e0512ac4c49000888c1e69f55
AlekseyArhipenko <arhipenko1990@inbox.ru>
2022-03-08 10:38:40 +0300

    finish

--- src/test/java/ru/netology/CardWithDelivery.java
+++ src/test/java/ru/netology/CardWithDelivery.java
@@ -3,6 +3,8 @@ package ru.netology;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -11,7 +13,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -19,6 +20,10 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class CardWithDelivery {
 
+    @BeforeAll
+    public static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
 
     @BeforeEach
     void setUp() {
